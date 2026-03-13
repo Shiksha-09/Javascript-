88dd5c8ed5bb20316251c62a658dbb700a26caf4
Shiksha-09 <shiksharawat092005@gmail.com>
2026-03-13 20:55:36 +0530

    day1

---
+++ BOM.js
@@ -0,0 +1,28 @@
+// function Check(){
+//     console.log("Shiksha")
+// }
+
+// Check()
+
+// function Add(){
+//     var a =15
+//     var b = 20
+//     console.log(a+b)
+// }
+//   Add()
+
+//   function subs(){
+//     var a= 50
+//     var b = 10
+//     console.log(a-b)
+//   }
+//   subs()
+
+  function BOM(){
+
+    var firstname = prompt("Enter your first name ")
+    var lastname = prompt("Enter last name ")
+    alert ("Welcome " +firstname+ " " +lastname)
+
+  }
+  BOM()
