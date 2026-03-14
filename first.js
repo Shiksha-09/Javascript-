d5a7f9b1465a239b3c68619d99d6c53cffd0952d
Shiksha-09 <shiksharawat092005@gmail.com>
2026-03-14 11:34:06 +0530

    edit

---
+++ first.js
@@ -0,0 +1,7 @@
+function Add(){
+    var num1 = Number(prompt("Enter first number "))
+      var num2 = Number(prompt("Enter second number "))
+      var sum= num1+num2
+      alert(" Sum of two number is " +sum )
+}
+Add()
