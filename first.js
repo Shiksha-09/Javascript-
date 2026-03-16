c15df47bfe73ee97420a79ff3c7a075087efc681
Shiksha-09 <shiksharawat092005@gmail.com>
2026-03-16 11:38:33 +0530

    if-else

--- first.js
+++ first.js
@@ -1,7 +1,33 @@
-function Add(){
-    var num1 = Number(prompt("Enter first number "))
-      var num2 = Number(prompt("Enter second number "))
-      var sum= num1+num2
-      alert(" Sum of two number is " +sum )
+// function Add(){
+//     var num1 = Number(prompt("Enter first number "))
+//       var num2 = Number(prompt("Enter second number "))
+//       var sum= num1+num2
+//       alert(" Sum of two number is " +sum )
+// }
+// Add()   
+
+function Condition(){
+  // var age = prompt("Enter your age")
+  // if(age>=18){
+  //   alert("You can Vote")
+  // }
+  // else{
+  //   alert("You cant Vote")
+  // }
+
+    var num= Number(prompt("Enter number "))
+//     if(num%2==0){
+//       alert("Even number")
+//     }
+//     else{
+//       alert("Odd number")
+//     }
+// }
+  if(num > 0){
+      alert("Positive")
+    }
+    else{
+      alert("Negative")
+    }
 }
-Add()
+Condition()
