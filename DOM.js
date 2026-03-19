671d38f16eafbbd378abbbc5fea2aac92fe8dd94
Shiksha-09 <shiksharawat092005@gmail.com>
2026-03-19 23:22:54 +0530

    Arrayprogram

---
+++ DOM.js
@@ -0,0 +1,12 @@
+// function Sid(){
+// //   
+// let Text= document.querySelector('.text1')
+// Text.innerHTML="This <i>text </i> is from Js"
+
+// Text.style.color="red"
+// }
+
+function Name(){
+    let Msg=document.querySelector('.msg')
+    Msg.style.display="none"
+}
