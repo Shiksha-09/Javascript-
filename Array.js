6212721c067a4e4ea6fddf2d968c8ee014c1b4fb
Shiksha-09 <shiksharawat092005@gmail.com>
2026-03-20 16:53:42 +0530

    DOM

--- Array.js
+++ Array.js
@@ -1,17 +1,17 @@
 function Arr(){
-    // let fruits =["Apple", "Mango","Banana", 9,85]
-    // console.log(fruits[4]) // access
+    let fruits =["Apple", "Mango","Banana", 9,85]
+    console.log(fruits[4]) // access
 
-    // console.log(fruits.length) //length
+    console.log(fruits.length) //length
 
-    // fruits.pop()
-    // fruits.push("Papaya", "Pineapple")
+    fruits.pop()
+    fruits.push("Papaya", "Pineapple")
 
-    // fruits.shift()
-    // fruits.unshift("Grapes")
-    // console.log(fruits.splice(2,4))
-    // fruits.splice(2,0,"papaya")
-    // console.log(fruits)
+    fruits.shift()
+    fruits.unshift("Grapes")
+    console.log(fruits.splice(2,4))
+    fruits.splice(2,0,"papaya")
+    console.log(fruits)
 
     //name reverse
      let  str="cybrom"
