4db8135167da54b1390decd37207d15e98a35433
Shiksha-09 <shiksharawat092005@gmail.com>
2026-03-25 14:41:37 +0530

    DOM

---
+++ function.js
@@ -0,0 +1,16 @@
+//Arrow function
+
+// let App= ()=>{
+//         let a=5
+//         let b=7
+
+//         return a+b
+// }
+
+// console.log(App())
+
+let area =(a)=>{
+   
+    return a*a
+}
+console.log(area(5))
